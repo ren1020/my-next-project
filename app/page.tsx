065a255f994703da760da4691460fe1ea04cd0ca
ren1020 <rem1020okita@gmail.com>
2025-10-25 15:50:56 +0900

    ホームページにニュースセクションを追加

--- app/page.tsx
+++ app/page.tsx
@@ -42,6 +42,7 @@ const data: {contents: News[]} = {
 
 export default function Home() {
   return (
+    <>
     <section className={styles.top}>
       <div>
         <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
@@ -57,5 +58,40 @@ export default function Home() {
         height={1200}
       />
     </section>
+    <section className={styles.news}>
+      <h2 className={styles.newsTitle}>News</h2>
+      <ul>
+        {data.contents.map((article) => (
+          <li key={article.id} className={styles.list}>
+            <div className={styles.link}>
+              <Image
+                className={styles.image}
+                src="/no-image.png"
+                alt="No Image"
+                width={1200}
+                height={630}
+              />
+              <dl className={styles.contents}>
+                <dt className={styles.newsItemTitle}>{article.title}</dt>
+                <dd className={styles.meta}>
+                  <span className={styles.tag}>{article.category.name}</span>
+                  <span className={styles.date}>
+                    <Image
+                      src="/clock.svg"
+                      alt=""
+                      width={16}
+                      height={16}
+                      priority
+                    />
+                    {article.publishedAt}
+                  </span>
+                </dd>
+              </dl>
+            </div>
+          </li>
+        ))} 
+      </ul>
+    </section>
+  </>
   );
 }
