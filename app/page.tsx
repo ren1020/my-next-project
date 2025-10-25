3090e9aab964050e1bbb0894457a769fa66d71dc
ren1020 <rem1020okita@gmail.com>
2025-10-25 14:42:27 +0900

    B

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
 export default function Home() {
-  return <h1>テクノロジーの力で世界を変える</h1>;
+  return (
+    <div>
+      <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+      <p className={styles.description}>
+        私たちは市場をリードしているグローバルカンパニーです。
+      </p>
+    </div>
+  );
 }
