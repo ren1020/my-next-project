be290cd4b79b1144180342b5121a301072677282
ren1020 <rem1020okita@gmail.com>
2025-10-25 15:27:28 +0900

    C

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,61 @@
 import styles from './page.module.css';
+import Image from 'next/image';
+
+type News = {
+  id: string;
+  title: string;
+  category: {
+    name: string;
+  };
+  publishedAt: string;
+  createdAt: string;
+}
+
+const data: {contents: News[]} = {
+  contents: [
+    {
+      id: "1", title: "渋谷のオフィスを移転しました",
+      category: { 
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+    {id: "2", 
+      title: "当社CEOが業界リーダーTOP30に選出されました",
+      category: { 
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+    {id: "3",
+      title: "テストの記事です",
+      category: { 
+        name: "更新情報",
+      },
+      publishedAt: "2023/04/19",
+      createdAt: "2023/04/19",
+    },
+  ],
+};
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
-      <p className={styles.description}>
-        私たちは市場をリードしているグローバルカンパニーです。
-      </p>
-    </div>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+        <p className={styles.description}>
+          私たちは市場をリードしているグローバルカンパニーです。
+        </p>
+      </div>
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+        width={4000}
+        height={1200}
+      />
+    </section>
   );
 }
