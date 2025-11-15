005bb826529e5c6a90388cf76b9bf72cfa41e78b
ren1020 <rem1020okita@gmail.com>
2025-11-15 15:26:23 +0900

    3

--- app/members/layout.tsx
+++ app/members/layout.tsx
@@ -1,9 +1,15 @@
 import Sheet from "@/app/_components/Sheet";
+import Hero from "@/app/_components/Hero";
 
 type Proos = {
     children: React.ReactNode;
 };
 
 export default function RootLayout({ children }: Proos) {
-    return <Sheet>{children}</Sheet>;
+    return (
+        <>
+          <Hero title="Members" sub="メンバー" />
+          <Sheet>{children}</Sheet>
+        </>
+    );
 }
