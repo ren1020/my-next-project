103f904cf26c07792e0e7e992f8a3b4f7d60c291
ren1020 <rem1020okita@gmail.com>
2025-11-29 15:33:07 +0900

    7

--- app/_components/Date/index.tsx
+++ app/_components/Date/index.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import styles from "./index.module.css";
+import timezone from "dayjs/plugin/timezone";
+import { formataDate } from "@/app/_libs/utils";
 
 type Props = {
     date: string;
@@ -9,7 +11,7 @@ export default function Date({ date }: Props) {
     return (
         <span className={styles.date}>
             <Image src="/clock.svg" alt="" width={16} height={16} loading="eager" />
-            {date}
+            {formataDate(date)}
         </span>
     );
 }
