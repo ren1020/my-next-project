51f0472e134a3fe43215103b68dbf353c8ae0eea
ren1020 <rem1020okita@gmail.com>
2025-11-29 16:17:45 +0900

    9

---
+++ app/news/p/[current]/page.tsx
@@ -0,0 +1,19 @@
+import { getNewsList } from "@/app/_libs/microcms";
+import NewsList from "@/app/_components/NewsList";
+
+type Props = {
+    params: {
+        current: string;
+    }
+}
+
+export default async function Page({ params }: Props) {
+    const current = parseInt(params.current, 10);
+
+    const { contents: news } = await getNewsList({
+        limit: 10,
+        offset: 10 * (current - 1),
+    });
+
+    return <NewsList news={news} />;
+}
