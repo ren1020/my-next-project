9c46b5ef913c56b3fa60d1b03d32dcef1d19f807
ren1020 <rem1020okita@gmail.com>
2025-12-06 14:13:06 +0900

    11

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
 import { notFound } from "next/navigation";
 import NewsList from "@/app/_components/NewsList";
+import Pagination from "@/app/_components/Pagination";
 import Category from "@/app/_components/Category";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
@@ -13,7 +14,7 @@ type Props = {
 export default async function Page({ params }: Props) {
     const category = await getCategoryDetail(params.id).catch(notFound);
 
-    const { contents: news } = await getNewsList({
+    const { contents: news, totalCount } = await getNewsList({
         limit: NEWS_LIST_LIMIT,
         filters: `category[equals]${category.id}`,
     })
@@ -24,6 +25,10 @@ export default async function Page({ params }: Props) {
             <Category category={category} /> の一覧
         </p>
         <NewsList news={news} />
+        <Pagination
+            totalCount={totalCount}
+            basePath={`/news/category/${category.id}`}
+        />
         </>
     ); 
 }
